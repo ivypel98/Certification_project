c8d2c03f514b16aa0b59882f5358cbf5a74eb7cd
ivypel <agassiivy@gmail.com>
2022-09-26 19:12:10 +0000

    71,5%

--- index.php
+++ index.php
@@ -14,7 +14,6 @@
     <link rel="stylesheet" href="css/swiper-bundle.min.css" />
     <!-- CSS -->
     <link href="./css/style.css" rel="stylesheet" />
-    <link rel="stylesheet" href="./css/style2.css">
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 
