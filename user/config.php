548c6d8ffd4d98e0053add13a8edc6d54d32579e
ivypel98 <agassiivy@gmail.com>
2022-09-27 15:58:16 +0000

    connect to always data

--- user/config.php
+++ user/config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'members');
+define('DB_SERVER', 'mysql-noeliemodeshop.alwaysdata.net');
+define('DB_USERNAME', '282505');
+define('DB_PASSWORD', 'Liorah98@');
+define('DB_NAME', 'noeliemodeshop_data');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_NAME);
